1b24115a5e8446dd46a79081e9fe502af9acda70
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-01-20 12:56:51 -0300

    Adicionando a criacao da lista, funcao de adicionar um amigo e validacoes

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+//CRIANDO ARRAY PARA ARMAZENAR OS DADOS
+let listaSecreta = []
+
+function adicionarAmigo(){
+    //CAPTURANDO O VALOR NO INPUT DA LISTA
+    let campoAmigo = document.getElementById('amigo')
+    let nomeLista = campoAmigo.value
+
+    console.log('Valor do campo: ' + nomeLista)
+
+    //VALIDAÇÃO PARA QUE O VALOR RECEBIDO DO USUÁRIO NÃO SEJA NULL OU VAZIO
+    if(nomeLista == '' || nomeLista == null){
+        alert('Insira um nome válido na lista! 🫡')
+    }else{
+    //INSERINDO OS VALORES DO INPUT NO ARRAY CASO FOR VÁLIDO
+        listaSecreta.push(nomeLista)
+        console.log('Lista: '+ listaSecreta)
+    }
+    campoAmigo.value = '' //LIMPANDO O VALOR DO CAMPO PARA VAZIO APÓS ADICIONAR
+    
+}
