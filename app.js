90311bd940666b4a97ceb1fdde011a5f6ed76361
Pedro Lucas <devpedro37@gmail.com>
2025-01-22 20:26:17 -0300

    Adicionando funcao de sortear amigo secreto e inserindo na tela

--- app.js
+++ app.js
@@ -37,3 +37,24 @@ function percorrerListaSectreta(){
 
     listaAmigosHTML.innerHTML = listaHTML
 }
+
+// SELECIONANDO ALEATORIAMENTE PARA O SORTEIO
+function sortearAmigo(){
+
+    // verificando se o array de amigos não está vazio
+    if(listaSecreta == null || listaSecreta == undefined){
+        alert('A lista não possui valores válidos, possivelmente está vazia.')
+        return
+    }
+
+    let sorteio = Math.floor(Math.random() * listaSecreta.length)
+    console.log(`Amigo sorteado: ${listaSecreta[sorteio]}`)
+    
+
+    let mostrarResultado = document.getElementById('resultado')
+    mostrarResultado.innerHTML = ''
+
+     mostrarResultado.innerHTML = `<li>Seu amigo secreto sorteado foi: ${listaSecreta[sorteio]}</li>`
+
+     return mostrarResultado
+}
